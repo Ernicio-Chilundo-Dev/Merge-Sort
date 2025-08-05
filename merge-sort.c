dfef4eb3cf037282524a2a07028a52ad0bef291e
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-08-05 23:59:51 +0200

    add

--- merge-sort.c
+++ merge-sort.c
@@ -5,4 +5,14 @@ void merge(int arr, int left, int mid, int right)
 {
     int n1 = mid - left + 1;
     int n2 = right - mid;
+
+    // Temporary array
+    int L[n1], R[n2];
+
+    // Copy data to temp array
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+    
 }
